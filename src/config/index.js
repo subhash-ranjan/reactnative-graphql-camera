6dee9d5d644f042ada73b452ec84a092765b725c
sandy-dev <51679395+sandy-dev@users.noreply.github.com>
2020-06-25 15:39:36 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 
 const LOCAL_URL = 'http://localhost:5000'
-const LOCAL_URL_IP = 'http://172.20.10.8:5000'
-const LIVE_URL = 'https://cart.wiserows.com'
+const LOCAL_URL_IP = 'http://localhost:5000'
+const LIVE_URL = 'http://localhost:5000'
 
 const BASE_URL = LIVE_URL
 
